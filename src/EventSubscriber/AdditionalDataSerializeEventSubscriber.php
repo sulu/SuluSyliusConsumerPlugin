84bf07bf61b033fbbb84a903b1143387ce6dd204
Patrick Hopfner <patrick.hopfner@gmail.com>
2018-11-06 08:04:36 +0100

    Add extension points

---
+++ src/EventSubscriber/AdditionalDataSerializeEventSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\SyliusProducerPlugin\EventSubscriber;
+
+use JMS\Serializer\EventDispatcher\Events;
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
+use Sulu\SyliusProducerPlugin\Producer\AdditionalDataInterface;
+
+class AdditionalDataSerializeEventSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            [
+                'event' => Events::POST_SERIALIZE,
+                'format' => 'json',
+                'method' => 'onPostSerialize',
+            ],
+        ];
+    }
+
+    public function onPostSerialize(ObjectEvent $event): void
+    {
+        $object = $event->getObject();
+        if (!$object instanceof AdditionalDataInterface) {
+            return;
+        }
+
+        $event->getVisitor()->setData('additionalData', $object->getAdditionalData());
+    }
+}
